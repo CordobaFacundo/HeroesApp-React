44fa75d40c1ede9861b4e61cd9e85a825a610b97
unknown <facundo.cordoba2012@gmail.com>
2023-05-27 11:56:45 -0300

    first commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export const DcPage = () => {
+  return (
+    <>
+        <h1>DcPage</h1>
+    </>
+  )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { MarvelPage, DcPage } from '../heroes'
+import { LoginPage } from '../auth/pages/LoginPage'
+import { Navbar } from '../ui/components'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Navbar />
+        <Routes>
+            <Route path='/marvel' element={<MarvelPage />} />
+            <Route path='dc' element={<DcPage />}/>
+            <Route path='login' element={<LoginPage />}/>
+            <Route path='/' element={<Navigate to={"/marvel"}/>}/>
+        </Routes>
+    </>
+  )
+}
