c9dd890402d7df3a42c451aeaf455f367a41934e
unknown <facundo.cordoba2012@gmail.com>
2023-05-29 11:24:17 -0300

    HeroPage + styles

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -5,7 +5,7 @@ import { HeroesList } from '../components'
 export const DcPage = () => {
   return (
     <>
-        <h1>DC Comics</h1>
+        <h1 style={{color: "white"}}>DC Comics</h1>
         <hr />
         <HeroesList publisher={'DC Comics'}/>
         
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -5,7 +5,7 @@ import { HeroesList } from '../components'
 export const MarvelPage = () => {
   return (
     <>
-        <h1>Marvel Comics</h1>
+        <h1 style={{color: "white"}}>Marvel Comics</h1>
         <hr />
         <HeroesList publisher={'Marvel Comics'}/>
     </>
