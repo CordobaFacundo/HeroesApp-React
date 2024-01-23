b97466ffe699d48d0fe754d59183b95af021fa93
CordobaFacundo <facundo.cordoba2012@gmail.com>
2024-01-23 10:38:55 -0300

    logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -23,22 +23,27 @@ export const AuthProvider = ({ children }) => {
     const [ authState, dispatch ] = useReducer( authReducer, {}, init )  
   
     const login = ( name = '' ) => {
-
         const user= { id: 'ABC', name}
-        
         const action = {
             type: types.login,
             payload: user
         }
 
         localStorage.setItem('user', JSON.stringify(user));
-
         dispatch(action);
     }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = { type: types.logout };
+        dispatch(action);
+    }
+
     return (
     <AuthContext.Provider value={{
         ...authState,
-        login: login
+        login: login,
+        logout: logout
     }}>
        { children } 
     </AuthContext.Provider>
