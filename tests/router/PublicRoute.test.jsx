ab2c151d90835079c93a0dd39a2e7b6b99cce7d9
CordobaFacundo <facundo.cordoba2012@gmail.com>
2024-04-09 20:10:55 -0300

    test PublicRoute navegacion auth

--- tests/router/PublicRoute.test.jsx
+++ tests/router/PublicRoute.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { AuthContext } from "../../src/auth"
 import { PublicRoute } from "../../src/heroes/routes/PublicRoute"
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 
 describe('Pruebas en <PublicRoute />', () => {
@@ -19,6 +20,39 @@ describe('Pruebas en <PublicRoute />', () => {
             </AuthContext.Provider>
         );
         expect(screen.getByText('Ruta publica')).toBeTruthy();
+     });
+
+     test('Debe navegar si esta autenticado', () => { 
+        
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'Strider',
+                id: 'ABC123'
+            }
+        }
+        
+        render(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Ruta publica</h1>
+                            </PublicRoute>
+                        } />
+                        <Route path='marvel' element={ <h1>Pagina Marvel</h1>} />
+                    </Routes>
+                    
+                    <PublicRoute>
+                        <h1>Ruta publica</h1>
+                    </PublicRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect( screen.getByText('Pagina Marvel')).toBeTruthy();
+        
      });
 
 });
