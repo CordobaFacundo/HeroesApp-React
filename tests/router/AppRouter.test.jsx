0e7a71e976fa94645bea19c22dce478da37460d5
CordobaFacundo <facundo.cordoba2012@gmail.com>
2024-04-16 19:48:04 -0300

    test AppRouter

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react"
+import { AuthContext } from "../../src/auth/context/AuthContext"
+import { MemoryRouter } from 'react-router-dom'
+import { AppRouter } from "../../src/router/AppRouter"
+
+describe ('Pruebas en AppRouter', () => {
+
+    test('debe mostrar el login si no esta autenticado', () => { 
+
+        const contextValue = {
+            logged: false,
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/marvel']}>
+                <AuthContext.Provider value={ contextValue }>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        );
+
+        expect( screen.getAllByText('Login').length).toBe(2)
+     
+    });
+
+    test('Debe mostar el componente de marvel si esta autenticado', () => { 
+
+        const contextValue = {
+            logged: true,
+        }
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <AuthContext.Provider value={ contextValue }>
+                    <AppRouter />
+                </AuthContext.Provider>
+            </MemoryRouter>
+        );
+
+        expect( screen.getAllByText('Marvel').length).toBeGreaterThanOrEqual(1);
+
+
+    })
+
+})
